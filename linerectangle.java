fe80eb3e5177c8701a18a7be8e2bfc72f5615cf3
sudheer7175 <81158384+sudheer7175@users.noreply.github.com>
2021-03-22 16:44:48 +0530

    Add files via upload

---
+++ linerectangle.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.Applet.*;
+import java.awt.*;
+/*<APPLET CODE="Linesrectovals" width=500 ,height=400></*APPLET>
+public class Linesrectangleovals extends JApplet
+{
+public void paint(Graphics g)
+{
+super.paint(g);
+get.set colour(colour.red);
+get.draw line(5,30,350,30);
+get.set colour(colour.blue);
+get.draw rectangle(100,40,90,55);
+get.fill rectangle(100,40,90,55);
+get.set colour(clour.green);
+get. round rectangle(195,40,90,55);
+get.draw round rectangle(240,40,90,20,20,50);
+get.set colour(colour.orange);
+get.3D rectangle(5,100,90,55,true);
+get.fill 3D rectangle(100,100,90,55,false);
+get.set oval(colour.pink);
+get.draw oval(195,100,90,55);
+get.fill oval(290,100,90,55);
+}
+}
